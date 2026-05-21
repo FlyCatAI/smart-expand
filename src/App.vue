457ed06fb77705b5085051b78eee0569859c61c5
FlyCatAI <245554739@qq.com>
2026-05-21 15:00:58 +0800

    GRA-66 实现收入明细页 (#15)
    * GRA-66 implement income details page
    
    * style: polish income details page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <HistoryPerformance v-if="currentRoute === '/history-performance'" />
+  <IncomeDetails v-if="currentRoute === '/income-details'" />
+  <HistoryPerformance v-else-if="currentRoute === '/history-performance'" />
   <DynamicMessages v-else-if="currentRoute === '/dynamic-messages'" />
   <NoticeDetail v-else-if="currentRoute === '/notice-detail'" />
   <AcquiringWorkbench v-else />
@@ -10,21 +11,28 @@ import { onBeforeUnmount, onMounted, ref } from 'vue';
 import AcquiringWorkbench from './pages/acquiring-workbench/AcquiringWorkbench.vue';
 import DynamicMessages from './pages/dynamic-messages/DynamicMessages.vue';
 import HistoryPerformance from './pages/history-performance/HistoryPerformance.vue';
+import IncomeDetails from './pages/income-details/IncomeDetails.vue';
 import NoticeDetail from './pages/dynamic-messages/NoticeDetail.vue';
 
-const currentRoute = ref(window.location.pathname);
+function normalizePath(pathname: string) {
+  return pathname === '/' ? '/' : pathname.replace(/\/$/, '');
+}
+
+const currentRoute = ref(normalizePath(window.location.pathname));
 
 function syncRoute() {
-  currentRoute.value = window.location.pathname;
+  currentRoute.value = normalizePath(window.location.pathname);
 }
 
 onMounted(() => {
   window.addEventListener('popstate', syncRoute);
+  window.addEventListener('hzy:route-changed', syncRoute);
   window.addEventListener('hzy:navigation', syncRoute);
 });
 
 onBeforeUnmount(() => {
   window.removeEventListener('popstate', syncRoute);
+  window.removeEventListener('hzy:route-changed', syncRoute);
   window.removeEventListener('hzy:navigation', syncRoute);
 });
 </script>
