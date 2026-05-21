c9887747414383ce7ef41e09473df1ff9ff69946
FlyCatAI <245554739@qq.com>
2026-05-21 11:43:16 +0800

    Implement history performance page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <AcquiringWorkbench />
+  <HistoryPerformance v-if="currentRoute === '/history-performance'" />
+  <AcquiringWorkbench v-else />
 </template>
 
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 import AcquiringWorkbench from './pages/acquiring-workbench/AcquiringWorkbench.vue';
+import HistoryPerformance from './pages/history-performance/HistoryPerformance.vue';
+
+const currentRoute = ref(window.location.pathname);
+
+function syncRoute() {
+  currentRoute.value = window.location.pathname;
+}
+
+onMounted(() => {
+  window.addEventListener('popstate', syncRoute);
+  window.addEventListener('hzy:navigation', syncRoute);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('popstate', syncRoute);
+  window.removeEventListener('hzy:navigation', syncRoute);
+});
 </script>
